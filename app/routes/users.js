94f96c88ff5ea2fc35d488b97138b67cc7948f45
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-05 15:00:17 -0300

    Login Update

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
 const { JWT_TOKEN } = process.env
 
+require('dotenv').config()
 
 router.post('/register', async (req,res) => {
   const {name, email, password} = req.body
   const user = new User({ name, email, password })
+  
   try {
     await user.save()
     res.status(200).json(user)
@@ -19,21 +20,24 @@ router.post('/register', async (req,res) => {
 
 router.post('/login', async (req,res)=> {
   const { email, password } = req.body
+  
   try {
     let user = await User.findOne({ email })
+    
     if (!user)
       res.status(401).json({ error: 'Incorrect email or password' })
-    else {
+    
+    else 
       user.isCorrectPassword(password, (err, same) => {
         if (!same) {
           res.status(401).json({ error: 'Incorrect email or password'})
         }
+        
         else {
           const token = jwt.sign({ email }, JWT_TOKEN, { expiresIn: '1d' })
           res.json({ user: user, token: token })
         }   
       })
-    }
   } catch (error) {
     res.status(500).json({ error: 'Internal error please try again' })
   }
