58f0e4b9f41b0e80f6c3ca83479854e843130d9e
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-11 14:40:54 -0300

    Note Index, update and delete

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const withAuth = require('../middlewares/auth');
+const note = require('../models/note');
 var router = express.Router();
 const Note = require('../models/note')
 
@@ -21,6 +22,15 @@ router.post('/', withAuth, async (req, res) => {
     }
 })
 
+router.get('/', withAuth, async (req,res) => {
+    try {
+        const note = await Note.find({ author: req.user._id })
+        res.json(note)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
 router.get('/:id', withAuth, async (req,res) => {
     try {
         const { id } = req.params
@@ -32,6 +42,44 @@ router.get('/:id', withAuth, async (req,res) => {
             res.status(403).json({ error: 'Permission denied' })
     } catch (error) {
         res.status(500).json({ error: 'Problem to get a note' })
+    }
+})
+
+router.put('/:id', withAuth, async (req, res) => {
+    const { title, body } = req.body
+    const { id } = req.params
+
+    try {
+        const note = await Note.findById(id)
+        
+        if (isOwner(req.user, note)){
+            const note = await Note.findByIdAndUpdate( id, 
+                { $set: { title: title, body: body, updated_at: Date.now() } },
+                { upsert: true, 'new': true }
+            )
+            
+            res.json(note)
+        } else
+            res.status(403).json({ error: 'Permission denied' })
+
+    } catch (error) {
+        res.status(500).json({ error: 'Problem to update a note' })
+    }
+})
+
+router.delete('/:id', withAuth, async(req,res) => {
+    const { id } = req.params
+
+    try {
+        const note = await Note.findById(id)
+
+        if (note && isOwner(req.user, note)){
+            await note.delete()
+            res.status(402).json({ message: 'Note deleted' })
+        } else
+            res.status(403).json({ error: 'Permission denied' })
+    } catch (error) {
+        res.status(500).json('Problem to delete a note')
     }
 })
 
