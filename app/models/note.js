0d989f6871e13230fcd5759161ae7873c6e6a962
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2022-07-11 15:50:57 -0300

    API Complete

--- app/models/note.js
+++ app/models/note.js
@@ -24,4 +24,6 @@ const noteSchema = new mongoose.Schema({
     },
 });
 
+noteSchema.index({ 'title': "text", 'body': "text" })
+
 module.exports = mongoose.model("Note", noteSchema);
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -31,6 +31,20 @@ router.get('/', withAuth, async (req,res) => {
     }
 })
 
+router.get('/search', withAuth, async(req, res) => {
+    const { query } = req.query
+
+    try {
+        const note = await Note
+            .find({ author: req.user._id })
+            .find({ $text: { $search: query } })
+        
+        res.json(note)
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
 router.get('/:id', withAuth, async (req,res) => {
     try {
         const { id } = req.params
